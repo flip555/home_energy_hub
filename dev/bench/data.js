358d25bf3e20ed4751d059dad5069625a1f0d5e8
github-action-benchmark <github@users.noreply.github.com>
2025-02-26 20:15:13 +0000

    add Benchmark (pytest) benchmark result for b2357ac454712395ad7925e044a251079cdecbf5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740600683706,
+  "lastUpdate": 1740600913663,
   "repoUrl": "https://github.com/flip555/home_energy_hub",
   "entries": {
     "Benchmark": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00003219611885972016",
             "extra": "mean: 1.8400684856455756 msec\nrounds: 418"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "109870462+flip555@users.noreply.github.com",
+            "name": "flip555",
+            "username": "flip555"
+          },
+          "committer": {
+            "email": "109870462+flip555@users.noreply.github.com",
+            "name": "flip555",
+            "username": "flip555"
+          },
+          "distinct": true,
+          "id": "b2357ac454712395ad7925e044a251079cdecbf5",
+          "message": "Update test-and-scan.yml",
+          "timestamp": "2025-02-26T20:14:17Z",
+          "tree_id": "0743fb371108e5c164b9b7708d926a33901d71a1",
+          "url": "https://github.com/flip555/home_energy_hub/commit/b2357ac454712395ad7925e044a251079cdecbf5"
+        },
+        "date": 1740600913080,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/performance/test_benchmark.py::test_core_operations",
+            "value": 25364.728352078986,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000023285681015955606",
+            "extra": "mean: 39.42482592832642 usec\nrounds: 24128"
+          },
+          {
+            "name": "tests/performance/test_benchmark.py::test_data_processing",
+            "value": 1702.413304598101,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000008902609828568144",
+            "extra": "mean: 587.4014243774229 usec\nrounds: 1124"
+          },
+          {
+            "name": "tests/performance/test_benchmark.py::test_memory_operations",
+            "value": 532.8973034264183,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000011403357636230979",
+            "extra": "mean: 1.8765341719130668 msec\nrounds: 413"
           }
         ]
       }
